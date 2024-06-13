b4bd90c73bef332ab95d245714bd2f1a757d1588
K4ysal <129117832+K4ysal@users.noreply.github.com>
2024-06-13 12:30:08 -0500

    Update DAO_Libro.php

--- dao/DAO_Libro.php
+++ dao/DAO_Libro.php
@@ -5,11 +5,11 @@ require_once "modelo/Libro.php";
 class DAO_Libro 
 {
     var $cn;
+    
     public function listarLibros() {
         $cn = new conexion();
         $c = $cn->conecta();
         $libros = array();
-        #$idLib; $idEst; $idCat; $titulo; $descripcion; $autor; $fecPub;
         $sql = "select * from tb_libro";
         if ($result = $c->query($sql)) {
             while ($row = $result->fetch_array()) {
@@ -28,15 +28,105 @@ class DAO_Libro
         return $libros;
     }
 
-    public function consultarLibro($id) {}
+    public function consultarLibro($id) {
+        $cn = new conexion();
+        $c = $cn->conecta();
+        $libro = new Libro();
+        $sql = "select * from tb_libro where id_lib = ?";
+        $stm = $c->prepare($sql);
+        $result = $stm->execute(array($id));
+        if ($row = $result->fetch_array()) {
+            $libro->setIdLib($row[0]);
+            $libro->setIdEst($row[1]);
+            $libro->setIdCat($row[2]);
+            $libro->setTitulo($row[3]);
+            $libro->setDescripcion($row[4]);
+            $libro->setAutor($row[5]);
+            $libro->setFecPub($row[6]);
+        }
+        $cn->desconecta();
+        return $libro;
+        
+    }
 
-    public function consultarLibroPorEst($estado) {return $libros;}
+    public function consultarLibroPorEst($estado) {
+        $cn = new conexion();
+        $c = $cn->conecta();
+        $libros = array();
+        $sql = "select * from tb_libro where estado=?";
+        $stm = $c->prepare($sql);
+        if ($result = $stm->execute(array($estado))) {
+            while ($row = $result->fetch_array()) {
+                $libro = new Libro();
+                $libro->setIdLib($row[0]);
+                $libro->setIdEst($row[1]);
+                $libro->setIdCat($row[2]);
+                $libro->setTitulo($row[3]);
+                $libro->setDescripcion($row[4]);
+                $libro->setAutor($row[5]);
+                $libro->setFecPub($row[6]);
+                $libros[] = $libro;
+            }
+        }
+        $cn->desconecta();   
+        return $libros;
+    }
 
-    public function consultarLibroPorCat($categoria) {return $libros;}
+    public function consultarLibroPorCat($id_cat) {
+        $cn = new conexion();
+        $c = $cn->conecta();
+        $libros = array();
+        #$sql = "select * from tb_libro as tb inner join tb_categoria as tc on tb.id_cat = tc.id_cat where tc.categoria = ?";
+        $sql = "select * from tb_libro where id_cat = ?"
+        $stm = $c->prepare($sql);
+        if ($result = $stm->execute(array($id_cat))) {
+            while ($row = $result->fetch_array()) {
+                $libro = new Libro();
+                $libro->setIdLib($row[0]);
+                $libro->setIdEst($row[1]);
+                $libro->setIdCat($row[2]);
+                $libro->setTitulo($row[3]);
+                $libro->setDescripcion($row[4]);
+                $libro->setAutor($row[5]);
+                $libro->setFecPub($row[6]);
+                $libros[] = $libro;
+            }
+        }
+        $cn->desconecta(); 
+        return $libros;
+    }
     
-    public function consultarLibroPorAut($autor) {return $libros;}
+    public function consultarLibroPorAut($autor) {
+        $cn = new conexion();
+        $c = $cn->conecta();
+        $libros = array();
+        $sql = "select * from tb_libro where autor = ?"
+        $stm = $c->prepare($sql);
+        if ($result = $stm->execute(array($autor))) {
+            while ($row = $result->fetch_array()) {
+                $libro = new Libro();
+                $libro->setIdLib($row[0]);
+                $libro->setIdEst($row[1]);
+                $libro->setIdCat($row[2]);
+                $libro->setTitulo($row[3]);
+                $libro->setDescripcion($row[4]);
+                $libro->setAutor($row[5]);
+                $libro->setFecPub($row[6]);
+                $libros[] = $libro;
+            }
+        }
+        $cn->desconecta(); 
+        return $libros;
+    }
 
-    public function agregarLibro($libro) {}
+    public function agregarLibro($libro) {
+        $cn = new conexion();
+        $c = $cn->conecta();
+        $libros = array();
+        $sql = "insert into tb_libro values (?)";
+        $stm = $c->prepare($sql);
+        
+    }
 
     public function modificarLibro($libro) {}
 
