991f681d25ed80af59cf7c13a1a5021fa823d4cc
K4ysal <129117832+K4ysal@users.noreply.github.com>
2024-06-18 13:56:53 -0500

    Update 2 DAO_Prestamo.php

--- dao/DAO_Prestamo.php
+++ dao/DAO_Prestamo.php
@@ -5,7 +5,11 @@ require_once "modelo/Prestamo.php";
 class DAO_Prestamo
 {
     var $cn;
-    public function consultarLibroPorEst($estado) {
+
+    #Recordar: agrega el atributo '$estado' a la clase Prestamo
+    public function consultarPrestamo($id) {return $prestamo;}
+    
+    public function consultarPrestamoPorEst($estado) {
         $cn = new conexion();
         $c = $cn->conecta();
         $prestamos = array();
@@ -22,12 +26,36 @@ class DAO_Prestamo
                 $prestamo->setHorPre($row[4]);
                 $prestamo->setFecDev($row[5]);
                 $prestamo->setHorDev($row[6]);
+                $prestamo->setEstado($row[7])
                 $prestamos[] = $prestamo;
             }
         }
         $cn->desconecta();   
         return $prestamos;
     }
-    
+
+    public function agregarPrestamo($prestamo) {
+        $cn = new conexion();
+        $c = $cn->conecta();
+        $sql = "insert into tb_prestamo values (null, ?, ?, ?, ?, ?, ?, ?)";
+        $stm = $c->prepare($sql);
+        $bool = $stm->execute(array($prestamo->getIdPer(), $prestamo->getIdLib(), $prestamo->getFecPre(), $prestamo->getHorPre(), $prestamo->getFecDev(), $prestamo->getHorDev(), $prestamo->getEstado()));
+        if (!$bool) {
+            echo "Error al agregar prestamo";
+        }
+        $cn->desconecta();
+    }    
+
+    public function actualizarEstado($id, $estado) {
+        $cn = new conexion();
+        $c = $cn->conecta();
+        $sql = "update tb_prestamo set estado=? where id_pre=?";
+        $stm = $c->prepare($sql);
+        $bool = $stm->execute(array($estado, $id));
+        if (!$bool) {
+            echo "Error al actualizar el estado del prestamo";
+        }
+        $cn->desconecta();
+    }
 }
 ?>
