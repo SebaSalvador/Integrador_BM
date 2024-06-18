f8d0e7c22cbb1c266a34bb2d35aa955c045f9c80
K4ysal <jose.c.cayhualla.s@gmail.com>
2024-06-18 00:49:16 -0500

    Agregacion de libros en BD y portadas

--- bd/bdprestamos.sql
+++ bd/bdprestamos.sql
@@ -145,6 +145,22 @@ INSERT INTO tb_libro VALUES(100, 1, 10, 'El principito', 'El Principito narra la
 INSERT INTO tb_libro VALUES(101, 1, 11, 'Cosmos','Cosmos trata de la ciencia en su contexto humano más amplio y explica cómo la ciencia y la civilización se desarrollan conjuntamente. La obra aborda también el tema de las misiones espaciales destinadas a explorar los planetas más próximos a la Tierra, del origen de la vida, de la muerte del Sol, de la evolución de las galaxias y de los orígenes de la materia, los soles y los mundos, así como también los más recientes descubrimientos sobre la vida fuera de la Tierra.','Carl Sagan','1981-01-01');
 INSERT INTO tb_libro VALUES(102, 1, 12, 'Code Complete (2 Edition)', 'Ampliamente considerada una de las mejores guías prácticas de programación, el CODE COMPLETE original de Steve McConnell ha ayudado a los desarrolladores a escribir mejor software durante más de una década. Ahora, este libro clásico ha sido completamente actualizado y revisado con prácticas de vanguardia y cientos de nuevos ejemplos de código que ilustran el arte y la ciencia de la construcción de software.', 'Steve McConnell', '2004-07-07');
 
+INSERT INTO tb_libro VALUES(103, 1, 10, '1984', 'Una distopía clásica que explora el control totalitario y el poder del lenguaje para manipular la realidad y la percepción pública', 'George Orwell', '1949-06-08');
+INSERT INTO tb_libro VALUES(104, 1, 10, 'El nombre del viento', 'Esta novela de fantasía sigue la vida de Kvothe, un músico y mago legendario, explorando la importancia de la narrativa y la palabra en la construcción de identidades y leyendas.', 'Patrick Rothfuss', '2007-03-27');
+INSERT INTO tb_libro VALUES(105, 1, 10, 'El perfume', 'Una novela que examina el poder del olfato y cómo se entrelaza con la identidad y la percepción, utilizando un lenguaje evocador y poético.', 'Patrick Süskind', '1985-00-00');
+INSERT INTO tb_libro VALUES(106, 1, 10, 'El lector', 'Esta obra narra la relación entre un joven y una mujer mayor en la Alemania post-nazi, explorando temas de memoria, literatura y responsabilidad personal.', 'Bernhard Schlink', '1995-00-00');
+
+INSERT INTO tb_libro VALUES(107, 1, 11, 'Breves respuestas a las grandes preguntas', 'En este libro póstumo, Hawking aborda preguntas fundamentales sobre el universo, la inteligencia artificial, el futuro de la humanidad y otros temas científicos de gran relevancia.', 'Stephen Hawking', '2018-10-16');
+INSERT INTO tb_libro VALUES(108, 1, 11, 'Sapiens: De animales a dioses', 'Este libro ofrece una visión panorámica de la historia de la humanidad desde la evolución hasta la revolución científica, explorando cómo las ideas y los avances científicos han moldeado nuestra sociedad.', 'Yuval Noah Harari', '2011-00-00');
+INSERT INTO tb_libro VALUES(109, 1, 11, 'Cosmos', 'Una obra clásica que explora el universo desde una perspectiva científica y filosófica, abordando temas como el origen de la vida, el cosmos, y la búsqueda de vida extraterrestre.', 'Carl Sagan', '1980-00-00');
+INSERT INTO tb_libro VALUES(110, 1, 11, 'El gen: Una historia personal', 'Este libro ofrece una exploración profunda de la genética y cómo la comprensión del ADN ha transformado nuestra comprensión de la biología y la medicina.', 'Siddhartha Mukherjee', '2016-05-17');
+
+INSERT INTO tb_libro VALUES(111, 1, 12, 'Clean Code: A Handbook of Agile Software Craftsmanship', 'Este libro es un referente en el desarrollo de software, enfocado en principios y prácticas para escribir código limpio y mantenible.', 'Robert C. Martin', '2012-06-19');
+INSERT INTO tb_libro VALUES(112, 1, 12, 'Thinking, Fast and Slow', 'Este libro explora la psicología del juicio y la toma de decisiones, proporcionando ideas valiosas para cualquier persona involucrada en el diseño de sistemas o la optimización de procesos.', 'Daniel Kahneman', '2011-10-25');
+INSERT INTO tb_libro VALUES(113, 1, 12, 'The Design of Everyday Things', 'Un clásico en diseño de interacción y usabilidad, que explora cómo los objetos cotidianos están diseñados y cómo podrían mejorarse para facilitar una mejor experiencia de usuario.', 'Don Norman', '1988-00-00');
+INSERT INTO tb_libro VALUES(114, 1, 12, 'Introduction to Algorithms', 'Conocido como CLRS, este libro es una referencia estándar en el estudio de algoritmos y estructuras de datos en la informática y la ingeniería de software.', 'Thomas H. Cormen, Charles E. Leiserson y varios', '1989-00-00');
+
+
 DELETE FROM tb_usuario;
 /*
 Estados:
