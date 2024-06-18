f7f4c2e20a87691cf6f9d164618d95cfd1bab9f3
K4ysal <129117832+K4ysal@users.noreply.github.com>
2024-06-18 13:39:15 -0500

    Update 1 DAO_Prestamo.php

--- dao/DAO_Prestamo.php
+++ dao/DAO_Prestamo.php
@@ -4,7 +4,30 @@ require_once "modelo/Prestamo.php";
 
 class DAO_Prestamo
 {
-
+    var $cn;
+    public function consultarLibroPorEst($estado) {
+        $cn = new conexion();
+        $c = $cn->conecta();
+        $prestamos = array();
+        $sql = "select * from tb_prestamo where estado=?";
+        $stm = $c->prepare($sql);
+        $stm->execute(array($estado));
+        if ($result = $stm->get_result()) {
+            while ($row = $result->fetch_array()) {
+                $prestamo = new Prestamo();
+                $prestamo->setIdPre($row[0]);
+                $prestamo->setIdPer($row[1]);
+                $prestamo->setIdLib($row[2]);
+                $prestamo->setFecPre($row[3]);
+                $prestamo->setHorPre($row[4]);
+                $prestamo->setFecDev($row[5]);
+                $prestamo->setHorDev($row[6]);
+                $prestamos[] = $prestamo;
+            }
+        }
+        $cn->desconecta();   
+        return $prestamos;
+    }
     
 }
 ?>
