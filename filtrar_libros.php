d313915123d9f83917cda8c6251cdd756ffa5a71
SebaSalvador <sebasalvadorqt@gmail.com>
2024-06-19 02:14:53 -0500

    Se cambio el diseño del div de libros

--- filtrar_libros.php
+++ filtrar_libros.php
@@ -26,17 +26,32 @@ if (!empty($buscar)) {
 
 $result = $conn->query($sql);
 
+echo "<div class='row mb-4'>";
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
-        echo "<div class='card'>";
-        echo "<h2>".$row['titulo']."</h2>";
-        echo "<p>ID Libro: ".$row['id_lib']."</p>";
-        echo '<button type="button" onclick="javascript:openDetailBook(\'' . $row['id_lib'] . '\');">Click Me!</button>';
+        echo "<div class='col-xl-3 col-md-6 mb-4'>";
+        echo "<div class='card border-left-warning shadow h-100 py-2'>";
+        echo "<div class='card-body'>";
+        echo "<div class='row no-gutters align-items-center'>";
+        echo "<div class='col mr-2'>";
+        echo "<div class='text-xs font-weight-bold text-warning text-uppercase mb-1'>Pending Requests</div>";
+        echo "<div class='h5 mb-0 font-weight-bold text-gray-800'>" . $row['titulo'] . "</div>";
+        echo "<p>ID Libro: " . $row['id_lib'] . "</p>";
+        echo '<button type="button" onclick="javascript:openDetailBook(\'' . $row['id_lib'] . '\');">Ver Libro<i class="fa-solid fa-eye"></i></button>';
         echo "</div>";
+        echo "<div class='col-auto'>";
+        echo "<img src='galeria/" . $row['id_lib'] . ".jpg' alt='Carátula del libro' class='img-fluid' style='max-width: 50px;'>";
+        echo "</div>";
+        echo "</div>"; // Cierra row no-gutters align-items-center
+        echo "</div>"; // Cierra card-body
+        echo "</div>"; // Cierra card border-left-warning shadow h-100 py-2
+        echo "</div>"; // Cierra col-xl-3 col-md-6 mb-4
     }
 } else {
     echo "<p>No se encontraron resultados</p>";
 }
+echo "</div>"; // Cierra row mb-4
+
 
 $cn->desconecta();
 ?>
