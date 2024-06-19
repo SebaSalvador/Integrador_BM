0d7ccb9067c3df73113e45cd4a33d5894d1fad1a
K4ysal <jose.c.cayhualla.s@gmail.com>
2024-06-19 11:34:01 -0500

    Cambios importantes en la BD y modelos

--- bd/bdprestamos.sql
+++ bd/bdprestamos.sql
@@ -18,7 +18,9 @@ CREATE TABLE tb_persona (
     telefono		varchar(16)		NOT NULL,
     PRIMARY KEY(id_per)
 );
-
+/*
+- El estado del usuario cambia a penalizado solo si presenta alguna sancion activa
+*/
 CREATE TABLE tb_usuario (
 	id_per			integer			NOT NULL,
 	id_tipo			integer			NOT NULL,
@@ -28,28 +30,30 @@ CREATE TABLE tb_usuario (
     FOREIGN KEY(id_per) REFERENCES tb_persona(id_per),
     FOREIGN KEY(id_tipo) REFERENCES tb_tipoUsuario(id_tipo)
 );
+/*
+- Una persona solo tiene una sancion cuando entrega un libro fuera del plazo de prestamo o el libro presente una observacion
+- En caso de que el libro entregado presente una observacion los dias_san son 14 dias 
+- Calculo dias sancion, si es fuera de plazo:
+    dias_san = fec_ent - fec_dev (fechas traidas de tb_prestamo)
+- La sancion comienza desde la fecha de inicio indicada, pero los dias cuentan a partir del siguiente dia
+- Calculo fecha de finalizacion: (Ultima fecha de sancion)
+    fec_fin = fec_ini + $dias_san 
+Estados: (para controlar que se muestra al usuario)
+- Activo
+- Inactivo
 
+*/
 CREATE TABLE tb_sancion (
-	id_san			integer			NOT NULL,
+	id_san			integer			NOT NULL AUTO_INCREMENT,
     id_per			integer			NOT NULL,
     dias_san		integer			NOT NULL,
     fec_ini			date			NOT NULL,
+    fec_fin         date            NOT NULL,
     razon			varchar(32)		NOT NULL,
+    estado          varchar(16)     NOT NULL,
     PRIMARY KEY(id_san),
     FOREIGN KEY(id_per) REFERENCES tb_persona(id_per)
-);
-
-CREATE TABLE tb_mensaje (
-	id_men			integer			NOT NULL,
-    id_per			integer			NOT NULL,
-    asunto			varchar(32)		NOT NULL,
-    detalle			text			NOT NULL,
-    fec_env			date			NOT NULL,
-    hor_env			varchar(16)		NOT NULL,
-    estado			varchar(16)		NOT NULL,
-    PRIMARY KEY(id_men),
-    FOREIGN KEY(id_per) REFERENCES tb_persona(id_per)
-);
+)AUTO_INCREMENT = 10000;
 
 CREATE TABLE tb_estado (
 	id_est			integer			NOT NULL,
@@ -59,13 +63,13 @@ CREATE TABLE tb_estado (
 );
 
 CREATE TABLE tb_categoria (
-	id_cat			integer			NOT NULL,
+	id_cat			integer			NOT NULL AUTO_INCREMENT,
     nombre			varchar(32)		NOT NULL,
     PRIMARY KEY(id_cat)
-);
+)AUTO_INCREMENT = 10;
 
 CREATE TABLE tb_libro (
-	id_lib			integer			NOT NULL,
+	id_lib			integer			NOT NULL AUTO_INCREMENT,
     id_est			integer			NOT NULL,
     id_cat			integer			NOT NULL,
     titulo			varchar(32)		NOT NULL,
@@ -75,13 +79,19 @@ CREATE TABLE tb_libro (
     PRIMARY KEY(id_lib),
     FOREIGN KEY(id_est) REFERENCES tb_estado(id_est),
     FOREIGN KEY(id_cat) REFERENCES tb_categoria(id_cat)
-);
+)AUTO_INCREMENT = 100;
 /*
 Estados del prestamo:
 Pendiente : Comprende desde la fecha de prestamo hasta 2 horas max
 En curso : Comprende desde que el usuario recoja el libro hasta la fecha de devolucion
 Pendiente Devolucion: Comprende desde fecha de devolucion hasta que el usuario devuelve el libro 
 Finalizado: Se marca cuando el usuario devuelve el libro, sea a tiempo o con retraso
+
+- En prestamos en estado pendiente de entrega, los dias de sancion se calculan:
+    dias_san = fec_ent - fec_dev (no importa la hora de entrega)
+¿Que pasa si una persona entrega el libro el 15/06/2024 a las 18:00 PM, sabiendo que su fecha de devolucion es 
+el 15/06/2024 a las 16:00 PM? ¿Hay sancion? No
+¿Que pasa si una persona demora mucho en entregar un libro? Pasado 7 dias se le agrega una observacion de extraviado
 */
 CREATE TABLE tb_prestamo (
 	id_pre			integer       NOT NULL AUTO_INCREMENT,
@@ -90,30 +100,37 @@ CREATE TABLE tb_prestamo (
     fec_pre			date			NOT NULL,
     hor_pre			varchar(16)		NOT NULL,
     fec_dev			date			NOT NULL,
-    hor_dev			varchar(16)		NOT NULL,
+    hor_dev			varchar(16)		NOT NULL, 
     estado			varchar(32)		NOT NULL,
     PRIMARY KEY(id_pre),
     FOREIGN KEY(id_per) REFERENCES tb_persona(id_per),
     FOREIGN KEY(id_lib)	REFERENCES tb_libro(id_lib)
 ) AUTO_INCREMENT=1000;
+
 /*
 Descripciones:
 - El libro ha sido manipulado o rasgado
 - El libro fue extraviado
 
-Condiciones:
+Condiciones: (Los libros en estado recuperado ya no se muestran en empleado)
 - Inutilizable
 - En solucion
 - Recuperado
+
+- Solo los libros que no tengan observaciones o estan en condicion Recuperado pueden mostrarse a
+las personas
+- Un libro puede tener muchas observaciones en su vida util
+- Solo puede haber una observacion en condicion Inutilizable o En solucion por libro
 */
 CREATE TABLE tb_observacion (
+    id_obs          integer         NOT NULL AUTO_INCREMENT,
 	id_lib			integer			NOT NULL,
     descripcion		varchar(32)		NOT NULL,
     fec_obs			date			NOT NULL,
     condicion		varchar(16)		NOT NULL,
-    PRIMARY KEY(id_lib),
+    PRIMARY KEY(id_obs),
     FOREIGN KEY(id_lib) REFERENCES tb_libro(id_lib)
-);
+)AUTO_INCREMENT = 100000;
 
 
 /*Insertando datos a las tablas*/
@@ -126,11 +143,11 @@ INSERT INTO tb_tipousuario VALUES(2, 'Administrador', 'Todos los permisos');
 
 DELETE FROM tb_categoria;
 
-INSERT INTO tb_categoria VALUES(10,'Literarios y Linguisticos');
-INSERT INTO tb_categoria VALUES(11,'Cientificos');
-INSERT INTO tb_categoria VALUES(12,'Tecnicos');
-INSERT INTO tb_categoria VALUES(13,'Divulgativos');
-INSERT INTO tb_categoria VALUES(14,'Ilustrativos');
+INSERT INTO tb_categoria VALUES(null,'Literarios y Linguisticos');
+INSERT INTO tb_categoria VALUES(null,'Cientificos');
+INSERT INTO tb_categoria VALUES(null,'Tecnicos');
+INSERT INTO tb_categoria VALUES(null,'Divulgativos');
+INSERT INTO tb_categoria VALUES(null,'Ilustrativos');
 
 DELETE FROM tb_estado;
 /*
@@ -153,28 +170,25 @@ INSERT INTO tb_persona VALUES(52336803, 'PAOLO GABRIEL RODRIGEZ PEREZ', 22, 'rod
 INSERT INTO tb_persona VALUES(74588241, 'ERNESTO GARCIA MEDINA', 21, 'garciaernesto@hotmail.com', 'Vía Verónica 74', '902533687');
 INSERT INTO tb_persona VALUES(53123564, 'MARIA ELENA ZAPATA SANCHEZ', 20, 'zapatamariae@gmail.com', 'Riera Rebeca 14', '995236708');
 
-DELETE FROM tb_mensaje;
-
 DELETE FROM tb_libro;
 
-INSERT INTO tb_libro VALUES(100, 1, 10, 'El principito', 'El Principito narra la historia de un niño príncipe que vive en un pequeño asteroide y que cae a la Tierra, donde conoce a un piloto varado en el desierto. Ambos entablan una conversación en clave poética donde hablan de filosofía, de crítica social, del amor, del honor y de mucho de lo que nos hace humanos.','Antoine de Saint Exupery', '1951-09-20');
-INSERT INTO tb_libro VALUES(101, 1, 11, 'Cosmos','Cosmos trata de la ciencia en su contexto humano más amplio y explica cómo la ciencia y la civilización se desarrollan conjuntamente. La obra aborda también el tema de las misiones espaciales destinadas a explorar los planetas más próximos a la Tierra, del origen de la vida, de la muerte del Sol, de la evolución de las galaxias y de los orígenes de la materia, los soles y los mundos, así como también los más recientes descubrimientos sobre la vida fuera de la Tierra.','Carl Sagan','1981-01-01');
-INSERT INTO tb_libro VALUES(102, 1, 12, 'Code Complete (2 Edition)', 'Ampliamente considerada una de las mejores guías prácticas de programación, el CODE COMPLETE original de Steve McConnell ha ayudado a los desarrolladores a escribir mejor software durante más de una década. Ahora, este libro clásico ha sido completamente actualizado y revisado con prácticas de vanguardia y cientos de nuevos ejemplos de código que ilustran el arte y la ciencia de la construcción de software.', 'Steve McConnell', '2004-07-07');
+INSERT INTO tb_libro VALUES(null, 1, 10, 'El principito', 'El Principito narra la historia de un niño príncipe que vive en un pequeño asteroide y que cae a la Tierra, donde conoce a un piloto varado en el desierto. Ambos entablan una conversación en clave poética donde hablan de filosofía, de crítica social, del amor, del honor y de mucho de lo que nos hace humanos.','Antoine de Saint Exupery', '1951-09-20');
+INSERT INTO tb_libro VALUES(null, 1, 12, 'Code Complete (2 Edition)', 'Ampliamente considerada una de las mejores guías prácticas de programación, el CODE COMPLETE original de Steve McConnell ha ayudado a los desarrolladores a escribir mejor software durante más de una década. Ahora, este libro clásico ha sido completamente actualizado y revisado con prácticas de vanguardia y cientos de nuevos ejemplos de código que ilustran el arte y la ciencia de la construcción de software.', 'Steve McConnell', '2004-07-07');
 
-INSERT INTO tb_libro VALUES(103, 1, 10, '1984', 'Una distopía clásica que explora el control totalitario y el poder del lenguaje para manipular la realidad y la percepción pública', 'George Orwell', '1949-06-08');
-INSERT INTO tb_libro VALUES(104, 1, 10, 'El nombre del viento', 'Esta novela de fantasía sigue la vida de Kvothe, un músico y mago legendario, explorando la importancia de la narrativa y la palabra en la construcción de identidades y leyendas.', 'Patrick Rothfuss', '2007-03-27');
-INSERT INTO tb_libro VALUES(105, 1, 10, 'El perfume', 'Una novela que examina el poder del olfato y cómo se entrelaza con la identidad y la percepción, utilizando un lenguaje evocador y poético.', 'Patrick Süskind', '1985-00-00');
-INSERT INTO tb_libro VALUES(106, 1, 10, 'El lector', 'Esta obra narra la relación entre un joven y una mujer mayor en la Alemania post-nazi, explorando temas de memoria, literatura y responsabilidad personal.', 'Bernhard Schlink', '1995-00-00');
+INSERT INTO tb_libro VALUES(null, 1, 10, '1984', 'Una distopía clásica que explora el control totalitario y el poder del lenguaje para manipular la realidad y la percepción pública', 'George Orwell', '1949-06-08');
+INSERT INTO tb_libro VALUES(null, 1, 10, 'El nombre del viento', 'Esta novela de fantasía sigue la vida de Kvothe, un músico y mago legendario, explorando la importancia de la narrativa y la palabra en la construcción de identidades y leyendas.', 'Patrick Rothfuss', '2007-03-27');
+INSERT INTO tb_libro VALUES(null, 1, 10, 'El perfume', 'Una novela que examina el poder del olfato y cómo se entrelaza con la identidad y la percepción, utilizando un lenguaje evocador y poético.', 'Patrick Süskind', '1985-00-00');
+INSERT INTO tb_libro VALUES(null, 1, 10, 'El lector', 'Esta obra narra la relación entre un joven y una mujer mayor en la Alemania post-nazi, explorando temas de memoria, literatura y responsabilidad personal.', 'Bernhard Schlink', '1995-00-00');
 
-INSERT INTO tb_libro VALUES(107, 1, 11, 'Breves respuestas a las grandes preguntas', 'En este libro póstumo, Hawking aborda preguntas fundamentales sobre el universo, la inteligencia artificial, el futuro de la humanidad y otros temas científicos de gran relevancia.', 'Stephen Hawking', '2018-10-16');
-INSERT INTO tb_libro VALUES(108, 1, 11, 'Sapiens: De animales a dioses', 'Este libro ofrece una visión panorámica de la historia de la humanidad desde la evolución hasta la revolución científica, explorando cómo las ideas y los avances científicos han moldeado nuestra sociedad.', 'Yuval Noah Harari', '2011-00-00');
-INSERT INTO tb_libro VALUES(109, 1, 11, 'Cosmos', 'Una obra clásica que explora el universo desde una perspectiva científica y filosófica, abordando temas como el origen de la vida, el cosmos, y la búsqueda de vida extraterrestre.', 'Carl Sagan', '1980-00-00');
-INSERT INTO tb_libro VALUES(110, 1, 11, 'El gen: Una historia personal', 'Este libro ofrece una exploración profunda de la genética y cómo la comprensión del ADN ha transformado nuestra comprensión de la biología y la medicina.', 'Siddhartha Mukherjee', '2016-05-17');
+INSERT INTO tb_libro VALUES(null, 1, 11, 'Breves respuestas a las grandes preguntas', 'En este libro póstumo, Hawking aborda preguntas fundamentales sobre el universo, la inteligencia artificial, el futuro de la humanidad y otros temas científicos de gran relevancia.', 'Stephen Hawking', '2018-10-16');
+INSERT INTO tb_libro VALUES(null, 1, 11, 'Sapiens: De animales a dioses', 'Este libro ofrece una visión panorámica de la historia de la humanidad desde la evolución hasta la revolución científica, explorando cómo las ideas y los avances científicos han moldeado nuestra sociedad.', 'Yuval Noah Harari', '2011-00-00');
+INSERT INTO tb_libro VALUES(null, 1, 11, 'Cosmos', 'Una obra clásica que explora el universo desde una perspectiva científica y filosófica, abordando temas como el origen de la vida, el cosmos, y la búsqueda de vida extraterrestre.', 'Carl Sagan', '1980-00-00');
+INSERT INTO tb_libro VALUES(null, 1, 11, 'El gen: Una historia personal', 'Este libro ofrece una exploración profunda de la genética y cómo la comprensión del ADN ha transformado nuestra comprensión de la biología y la medicina.', 'Siddhartha Mukherjee', '2016-05-17');
 
-INSERT INTO tb_libro VALUES(111, 1, 12, 'Clean Code: A Handbook of Agile Software Craftsmanship', 'Este libro es un referente en el desarrollo de software, enfocado en principios y prácticas para escribir código limpio y mantenible.', 'Robert C. Martin', '2012-06-19');
-INSERT INTO tb_libro VALUES(112, 1, 12, 'Thinking, Fast and Slow', 'Este libro explora la psicología del juicio y la toma de decisiones, proporcionando ideas valiosas para cualquier persona involucrada en el diseño de sistemas o la optimización de procesos.', 'Daniel Kahneman', '2011-10-25');
-INSERT INTO tb_libro VALUES(113, 1, 12, 'The Design of Everyday Things', 'Un clásico en diseño de interacción y usabilidad, que explora cómo los objetos cotidianos están diseñados y cómo podrían mejorarse para facilitar una mejor experiencia de usuario.', 'Don Norman', '1988-00-00');
-INSERT INTO tb_libro VALUES(114, 1, 12, 'Introduction to Algorithms', 'Conocido como CLRS, este libro es una referencia estándar en el estudio de algoritmos y estructuras de datos en la informática y la ingeniería de software.', 'Thomas H. Cormen, Charles E. Leiserson y varios', '1989-00-00');
+INSERT INTO tb_libro VALUES(null, 1, 12, 'Clean Code: A Handbook of Agile Software Craftsmanship', 'Este libro es un referente en el desarrollo de software, enfocado en principios y prácticas para escribir código limpio y mantenible.', 'Robert C. Martin', '2012-06-19');
+INSERT INTO tb_libro VALUES(null, 1, 12, 'Thinking, Fast and Slow', 'Este libro explora la psicología del juicio y la toma de decisiones, proporcionando ideas valiosas para cualquier persona involucrada en el diseño de sistemas o la optimización de procesos.', 'Daniel Kahneman', '2011-10-25');
+INSERT INTO tb_libro VALUES(null, 1, 12, 'The Design of Everyday Things', 'Un clásico en diseño de interacción y usabilidad, que explora cómo los objetos cotidianos están diseñados y cómo podrían mejorarse para facilitar una mejor experiencia de usuario.', 'Don Norman', '1988-00-00');
+INSERT INTO tb_libro VALUES(null, 1, 12, 'Introduction to Algorithms', 'Conocido como CLRS, este libro es una referencia estándar en el estudio de algoritmos y estructuras de datos en la informática y la ingeniería de software.', 'Thomas H. Cormen, Charles E. Leiserson y varios', '1989-00-00');
 
 
 DELETE FROM tb_usuario;
